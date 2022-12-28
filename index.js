6046e84baefeeda89dfecda7b086a0cd1d4a09a5
NikitaBez <107099081+NikitaBez@users.noreply.github.com>
2022-12-28 13:11:14 +0000

    work program developer

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 "use strict";
 var _ = require('lodash');
 
-let a = 1; //Количество сотрудников A
+let a = 5; //Количество сотрудников A
 let salaryA = 10_000; // Зарплата сотрудника A
-let b = 2; //Количество сотрудников B
+let b = 15; //Количество сотрудников B
 let salaryB = 25_000; // Зарплата сотрудника B
-let c = 3; //Количество сотрудников C
+let c = 40; //Количество сотрудников C
 let salaryC = 50_000; // Зарплата сотрудника C
-let d = 4; //Количество сотрудников D
+let d = 10; //Количество сотрудников D
 let salaryD = 100_000; // Зарплата сотрудника D
-let tax = 0.2;
+
+let levelFreeTax; // ставка налога на первом этапе
+let level2Tax = 0.1; // ставка налога на втором этапе
+let level3Tax = 0.2; // ставка налога на третьем этапе
+let level4Tax = 0.5 // ставка налога на четвертом этапе
+let salaryFreeStep = 10_000; // налогонеоблагаемая сумма
+let salary2Level = 10_000; // налогооблагаемая сумма на втором этапе
+let salary3Level = 50_000; // налогооблагаемая сумма на третьем этапе
 
 let salary = [];
+let sumTax = [];
+let sumTax2 = [];
+let sumTax3 = [];
+let sumTax4 = [];
 for (let i = 0; i < a; i++) {
   salary.push(salaryA);
 }
@@ -24,14 +35,58 @@ for (let i = 0; i < c; i++) {
 for (let i = 0; i < d; i++) {
   salary.push(salaryD);
 }
-console.log(salary);
+console.log("Вывод основного массива: " + salary); //вывод массива первоначального
 
+console.log("============================================")
 
+for (let i = 0; i < salary.length; i++) {
+  if (salary[i] >= salaryFreeStep) {
+    salary[i] = salary[i] - salaryFreeStep;
+  }else{salary[i] = 0;}
+  console.log("Списание первых 10000 без налога: " + salary);
+}
 
+console.log("============================================")
 
-var salarySum = _.sum(salary);
-let sumTaxes = salarySum * tax;
+for (let i = 0; i < salary.length; i++) {
+  if (salary[i] <= salary2Level){
+    sumTax2[i] = salary[i] * level2Tax;
+  } if (salary[i] <= salary2Level) {salary[i] = 0;
+    }else{
+    sumTax2[i] = salary2Level * level2Tax;
+    salary[i] = salary[i] - salary2Level;
+  }
+  console.log("Вывод массива salary: " + salary);
+  console.log("Вывод массива sumTax: " + sumTax2);
+}
+
+console.log("============================================")
+
+for (let i = 0; i < salary.length; i++) {
+  if (salary[i] <= salary3Level){
+    sumTax3[i] = salary[i] * level3Tax;
+  } if (salary[i] <= salary3Level) {salary[i] = 0;
+    }else{
+    sumTax3[i] = salary3Level * level3Tax;
+    salary[i] = salary[i] - salary3Level;
+  }
+  console.log("Вывод массива salary: " + salary);
+  console.log("Вывод массива sumTax2: " + sumTax3);
+}
+console.log("============================================")
+
+for (let i = 0; i < salary.length; i++) {
+  if (salary[i] > 0){
+    sumTax4[i] = salary[i] * level4Tax;
+  } 
+  console.log("Вывод массива salary: " + salary);
+  console.log("Вывод массива sumTax4: " + sumTax4);
+}
+  console.log("============================================")
+
+var sumTaxes = _.sum(sumTax2)+_.sum(sumTax3)+_.sum(sumTax4);
 console.log("Суммарные налоговые отчисления со всех зарплат: " + sumTaxes + " рублей");
+
 let staff = [a, b, c, d];
 var staffSum = _.sum(staff);
 let averageTax = sumTaxes / staffSum;
